f113c5c6777798fe943bea3dc1ec296207aa4df3
unknown <Arun>
2024-10-24 22:39:52 +0530

    added annotation After for driver close and clean up

--- src/main/runner/TestRunner.java
+++ src/main/runner/TestRunner.java
@@ -1,24 +1,30 @@
 package main.runner;
 
+import io.cucumber.java.AfterAll;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.plugin.Plugin;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
      features = {"src/test/java/demoqa/features"},
      glue = { "test.java.demoqa.uiStepdefs" , "test.java.demoqa"},
 tags = "@RegTC",
-        plugin = {
+        plugin = {"pretty","html:test-output","json:json_output/cucumber.json","junit:junit_output/cucumber.xml"
 
         }
 
 
 
+
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+
+
 
 
 }
